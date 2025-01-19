28cb969520cea25fc80901adce2d831019b6a6a8
MrFliifi <felix-behrens@hotmail.de>
2025-01-19 18:59:58 +0100

    did basic ArrayHandler class

--- SoulsBingo/src/main/java/com/behrens/SoulsBingo/Backend/App.java
+++ SoulsBingo/src/main/java/com/behrens/SoulsBingo/Backend/App.java
@@ -4,16 +4,34 @@ import java.io.IOException;
 
 public class App 
 {
-	static String[] input = { "alle", "kinder", "lernen", "lesen"};
+	static String[] input = {"alle", "kinder", "lernen", "lesen", "schreiben",  
+			"spielen", "singen", "tanzen", "malen", "rechnen",  
+			"lachen", "weinen", "rennen", "springen", "fliegen",  
+			"denken", "schlafen", "essen", "trinken", "suchen",  
+			"finden", "bauen", "klettern", "fahren", "reisen",  
+			"fühlen", "hören", "sehen", "riechen", "sprechen",  
+			"verstehen", "erzählen", "kaufen", "verkaufen",  
+			"kochen", "backen", "sammeln", "zählen", "zeichnen",  
+			"schwimmen", "basteln", "wandern", "arbeiten", "leben",  
+			"lieben", "streiten", "rufen", "werfen", "fangen",  
+			"graben", "pflanzen", "gießen", "schneiden", "nähen",  
+			"binden", "kleben", "färben", "zuhören", "besuchen",  
+			"laden", "bügeln", "putzen", "wischen", "erinnern",  
+			"fragen", "antworten", "planen", "erfinden", "fotografieren",  
+			"filmen", "jubeln", "staunen", "schmunzeln", "erkunden",  
+			"erleben", "träumen", "experimentieren", "proben",  
+			"unterrichten", "forschen", "programmieren", "konstruieren",  
+			"zeichnen", "vergleichen", "bewerten", "tanzen", "lächeln",  
+			"gestalten", "organisieren", "backen", "kommunizieren", "meditieren"};
 	static String filePath = "NewFile.txt";
 	static String deleteStrin = "alle";
 	
     public static void main( String[] args ) throws IOException
     {		
     	FileHandler fileHandler = new FileHandler();
-        
-        fileHandler.writeMeAFile(filePath, input);
-        fileHandler.fetchEntries(filePath);
+        ArrayHandler arrayHandler = new ArrayHandler();
+
+        String[][] fullGrid = arrayHandler.setGridUp(filePath);
+        System.out.println();
     }
 }
-
--- SoulsBingo/src/main/java/com/behrens/SoulsBingo/Backend/ArrayHandler.java
+++ SoulsBingo/src/main/java/com/behrens/SoulsBingo/Backend/ArrayHandler.java
@@ -1,5 +1,51 @@
 package com.behrens.SoulsBingo.Backend;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class ArrayHandler {
 
+	public String[][] setGridUp(String filePath) throws IOException {
+		String[][] grid = createGrid();
+		String[][] fullGrid = fillGrid(grid, filePath);
+		return fullGrid;
+	}
+
+	private String[][] createGrid() {
+		String[][] grid = new String[5][5];
+		return grid;
+	}
+
+	private String[][] fillGrid(String[][] grid, String filePath) throws IOException {
+		Random random = new Random();
+		FileHandler fileHandler = new FileHandler();
+		String[] entries = fileHandler.fetchEntries(filePath);
+
+		if (entries.length < grid.length * grid[0].length) {
+			throw new IllegalArgumentException("Not enough unique entries to fill the grid.");
+		}
+
+		Set<Integer> usedIndices = new HashSet<>();
+		int totalCells = grid.length * grid[0].length;
+
+		for (int i = 0; i < totalCells; i++) {
+			int x;
+			do {
+				x = random.nextInt(entries.length);
+			} while (usedIndices.contains(x));
+
+			usedIndices.add(x);
+
+			// Fill the grid
+			grid[i / grid[0].length][i % grid[0].length] = entries[x];
+		}
+		return grid;
+	}
+
+	public boolean[][] gridIsClicked(){
+		boolean[][] isClicked = new boolean[5][5];
+		return isClicked;
+	} 
 }
